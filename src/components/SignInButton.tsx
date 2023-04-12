3700b5c0b5ac56b76a993461b622bf3183374416
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-04-12 19:59:21 +0530

    remove toast to counter the next error.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -2,7 +2,6 @@
 import {FC, useState} from 'react'
 import Button from "@/ui/Button";
 import {signIn} from "next-auth/react";
-import {toast} from "@/ui/Toast"
 
 interface SignInButtonProps {
 }
@@ -13,11 +12,11 @@ const SignInButton: FC<SignInButtonProps> = ({}) => {
     const signInWithGoogle = async () => {
         setLoading(true)
         try {
-            toast({
-                type: "default",
-                title: "Signing in",
-                message: "!!."
-            })
+            // toast({
+            //     type: "default",
+            //     title: "Signing in",
+            //     message: "!!."
+            // })
             await signIn("Google")
         } catch (error) {
             console.log(error)
