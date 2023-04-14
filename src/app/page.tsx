ed8590e8bad6f40b85def8d065332b840965a895
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-04-14 15:54:08 +0530

    add large heading's content.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,9 @@ export default function Home() {
         <div className={"relative h-screen flex items-center justify-center overflow-x-hidden"}>
             <div className="container pt-32 max-w-7xl mx-auto w-full h-full">
                 <div className="h-full gap-6 flex flex-col justify-start lg:justify-center items-center lg:items-start">
-                    <LargeHeading size={"large"} className={"three-d text-black dark:text-light-gold"}></LargeHeading>
+                    <LargeHeading size={"large"} className={"three-d text-black dark:text-light-gold"}>
+                        Easily Determine <br/> text similarity
+                    </LargeHeading>
                 </div>
             </div>
         </div>
