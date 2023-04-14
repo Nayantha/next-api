5fd4fe1226c84c00fe54be34ee8cb7b59e51fbe5
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-04-14 15:49:41 +0530

    add metadata.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: "API | Home",
+    description: "REST API with next.js"
+}
 export default function Home() {
     return (
-        <main></main>
+        <></>
     )
 }
