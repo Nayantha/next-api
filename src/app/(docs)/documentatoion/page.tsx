ee3fbf1551e6b528891fd7fbe358f5ae9bb378de
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-04-14 16:32:19 +0530

    edit metadata to document page.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- src/app/(docs)/documentatoion/page.tsx
+++ src/app/(docs)/documentatoion/page.tsx
@@ -2,8 +2,8 @@ import {FC} from 'react'
 import {Metadata} from "next";
 
 export const metadata: Metadata = {
-    title: "API | Home",
-    description: "REST API with next.js"
+    title: "API | Documentation",
+    description: "Documentation of the REST API with next.js"
 }
 const page: FC = ({}) => {
     return <div>page</div>
